422aef75c1e2e8e587a76d9a425c45885c416df3
yuki.yoshida <sis.yoshiday@gmail.com>
2014-10-16 15:53:56 +0900

    initial commit

---
+++ step1/src/main/java/com/example/amazon/capitolhillcofee/CoffeeActivity.java
@@ -0,0 +1,33 @@
+package com.example.amazon.capitolhillcofee;
+
+import android.os.Bundle;
+import com.amazon.geo.maps.MapActivity;
+import com.amazon.geo.maps.MapView;
+
+/**
+ * Activity class for coffee shop app.
+ */
+public class CoffeeActivity extends MapActivity {
+  private MapView mMapView;
+
+  /**
+   * Upon creation, this demo app:
+   * - creates a stock mapview and turns on the zoom control widget
+   */
+  @Override public void onCreate(final Bundle icicle) {
+    super.onCreate(icicle);
+
+    setContentView(R.layout.main);
+
+    // Extract a reference to the map view
+    mMapView = (MapView) findViewById(R.id.mapview);
+
+    // Turn on the zoom control widget
+    mMapView.setBuiltInZoomControls(true);
+  }
+
+  // Override required by interface
+  @Override protected boolean isRouteDisplayed() {
+    return false;
+  }
+}
